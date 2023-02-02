7359c6f6fe79718cc5060c5a71f4b800c91b1a10
luis-arocho <94925398+luis-arocho@users.noreply.github.com>
2023-02-02 08:37:43 -0500

    Update testMashup.js

--- testMashup.js
+++ testMashup.js
@@ -7,11 +7,11 @@
  */
 var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
 var config = {
-	host: 'e7ixql68q51ilev.us.qlikcloud.com',
+	host: 'koa48q35agc9ri4.us.qlikcloudgov.com',
 	prefix: '/',
 	port: 443,
 	isSecure: window.location.protocol === "https:",
-	webIntegrationId: 'xV3eqA-sz5oEoQbji7YJw1JCQ0nzqvLj'
+	webIntegrationId: 'Mm96Y7FJbRgW9f50grpGEXJGV0YITgEP'
 };
 //Redirect to login if user is not logged in
 async function login() {
@@ -50,15 +50,15 @@ require( ["js/qlik"], function ( qlik ) {
 		$( '#popup' ).hide();
 	} );
     //open apps -- inserted here --
-	var app = qlik.openApp( '831d3888-5b6f-4232-9e9e-7c1903a543ce', config );
+	var app = qlik.openApp( 'bc1ca4ce-4e73-4190-a0a6-827ba3197960', config );
 	
     //get objects -- inserted here --
-	app.visualization.get('MCpCLf').then(function(vis){
+	app.visualization.get('mdjrnd').then(function(vis){
     vis.show("QV01");	
 	} );
     
     //get objects -- inserted here --
-	app.visualization.get('KgjnT').then(function(vis){
+	app.visualization.get('YbwXW').then(function(vis){
     vis.show("QV02");	
 	} );	
 } );});
